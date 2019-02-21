9b66573ed252a232e67aebfb87d8e35f6405923a
chubibest <chubi.best@gmail.com>
2019-02-21 20:10:23 +0100

    Apply babel and eslint configurations

--- playground/pg.connect.js
+++ playground/pg.connect.js
@@ -1,6 +1,10 @@
 const { Client } = require('pg')
+require ('dotenv').config()
 const client = new Client({
     connectionString: process.env.DATABASE_URL
 });
-client.connect().then(()=>{console.log('connected')},(e)=>{console.log(e)})
 client.on('connect' , ()=> {console.log('connected to server')})
+client.connect().then(()=>{console.log('connected')},(e)=>{console.log(e)})
+const query = `CREATE TABLE IF NOT EXISTS todo(id varchar(20) PRIMARY KEY,item varchar(20),completed boolean)` 
+client.query(query).then((res)=>console.log(res)).catch((e)=>console.log(e))
+console.log('hmmn')
