7e26e11103f1c34ecbc3f2b3d58bd4eee4dc4c4b
chubibest <chubi.best@gmail.com>
2019-02-22 09:50:50 +0100

    Save some progress

--- playground/pg.connect.js
+++ playground/pg.connect.js
@@ -5,6 +5,11 @@ const client = new Client({
 });
 client.on('connect' , ()=> {console.log('connected to server')})
 client.connect().then(()=>{console.log('connected')},(e)=>{console.log(e)})
-const query = `CREATE TABLE IF NOT EXISTS todo(id varchar(20) PRIMARY KEY,item varchar(20),completed boolean)` 
-client.query(query).then((res)=>console.log(res)).catch((e)=>console.log(e))
+// const query = `CREATE TABLE IF NOT EXISTS todo(id SERIAL PRIMARY KEY,item varchar(20),completed boolean)` 
+// const query = {
+//     text : " INSERT INTO todo  (item, completed) values ($1,$2) returning *",
+//     values : ['die',false]
+// }
+const query = `SELECT COUNT(id) FROM todo WHERE  completed = true   `
+client.query(query).then((res)=>console.log(res.rows)).catch((e)=>console.log(e))
 console.log('hmmn')
