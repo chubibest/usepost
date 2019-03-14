371b1277dc55de8cbf34b792bbe9bd15bf3ef4ea
chubibest <chubi.best@gmail.com>
2019-03-14 21:32:47 +0100

    fix shit

--- createTable.js
+++ createTable.js
@@ -6,29 +6,25 @@ dotenv.config();
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL
 });
-const connect = pool.connect();
+const query = queryText => pool.connect()
+  .then(client => client.query(queryText).then(() => client.release(), () => client.release())
+    .catch(() => client.release()));
 
 
 const createUserTable = () => {
-  const query = `CREATE TABLE IF NOT EXISTS todoes (
+  const queryText = `CREATE TABLE IF NOT EXISTS todoes (
      id serial primary key,
      item varchar(20) not null unique,
      created_at timestamptz not null default now(),
      completed boolean default false, 
       completed_at timestamptz default null)`;
-  connect.then(client => client.query(query).then((res) => {
-    client.release();
-    console.log('this table was created', res);
-  }, e => console.log(e)), e => console.log(e));
+  query(queryText);
 };
 
 
 const dropUserTable = () => {
   const queryText = 'DROP TABLE IF EXISTS todoes;';
-  connect.then(client => client.query(queryText).then((res) => {
-    client.release();
-    console.log('this is the result', res);
-  }, e => console.log(e))).catch(e => console.log(e));
+  query(queryText);
 };
 
 module.exports = { createUserTable, dropUserTable };
