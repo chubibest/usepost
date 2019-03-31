314984a217640d0b71ee36ddc3d0c86de076497a
chubibest <chubi.best@gmail.com>
2019-03-31 15:46:05 +0100

    serve static html file

--- server/views/main.js
+++ server/views/main.js
@@ -1,26 +1,25 @@
 const join = document.querySelector('#join');
-const login = document.querySelector('#login')
-const signUpDiv = document.querySelector("#signUpDiv");
-const loginDiv = document.querySelector("#loginDiv");
-const opaque = document.querySelectorAll(".opacity")
-const closeSignUp = document.querySelector('#closeSignUp')
-const closeLogin = document.querySelector('#closeLogin')
+const login = document.querySelector('#login');
+const signUpDiv = document.querySelector('#signUpDiv');
+const loginDiv = document.querySelector('#loginDiv');
+const opaque = document.querySelectorAll('.opacity');
+const closeSignUp = document.querySelector('#closeSignUp');
+const closeLogin = document.querySelector('#closeLogin');
 
 
 const close = (div) => {
   opaque.forEach((n) => {
-    n.style.display = 'unset'
+    n.style.display = 'unset';
   });
-  div.style.display = 'none'
-}
-const display= (div) => {
-
-  div.style.display = 'block';
-  opaque.forEach((n)=>{
-    n.style.display = 'none'
-  })
+  div.style.display = 'none';
 };
-join.addEventListener('click', () => {display(signUpDiv)})
-login.addEventListener('click', () => {display(loginDiv)})
-closeSignUp.addEventListener('click', () => { close(signUpDiv)})
-closeLogin.addEventListener('click', () => { close(loginDiv)})
+const display = (div) => {
+  div.style.display = 'block';
+  opaque.forEach((n) => {
+    n.style.display = 'none';
+  });
+};
+join.addEventListener('click', () => { display(signUpDiv); });
+login.addEventListener('click', () => { display(loginDiv); });
+closeSignUp.addEventListener('click', () => { close(signUpDiv); });
+closeLogin.addEventListener('click', () => { close(loginDiv); });
